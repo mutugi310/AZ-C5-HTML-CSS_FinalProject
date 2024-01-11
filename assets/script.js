4e0d12cccfa399929414e34f26f5a7dd4e5f8c3c
kelvinmutugi210 <kelvinmutugi210@gmail.com>
2024-01-11 03:49:36 +0300

    Fix the hamburger menu

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const overlayContainer = document.querySelector('.overlay-container');
-    const openOverlayButton = document.querySelector('.open-overlay-button');
-    const closeOverlayButton = document.querySelector('.X-icon');
-  
-    openOverlayButton.addEventListener('click', function () {
-      overlayContainer.style.display = 'block';
-    });
-  
-    closeOverlayButton.addEventListener('click', function () {
-      overlayContainer.style.display = 'none';
-    });
+  const overlayContainer = document.querySelector('.overlay-container');
+  const openOverlayButton = document.querySelector('.hamburger-btn');
+  const closeOverlayButton = document.querySelector('.X-icon');
+
+  openOverlayButton.addEventListener('click', function () {
+    overlayContainer.style.display = 'block';
   });
-  
+
+  closeOverlayButton.addEventListener('click', function () {
+    overlayContainer.style.display = 'none';
+  });
+});
